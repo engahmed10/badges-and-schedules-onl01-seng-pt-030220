32c6c4ec7722ec0ac409e2a4a64091f862d62497
AHMED AFANDY <eng.computer1@yahoo.com>
2020-03-04 23:45:29 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
 # Write your code here.
+def badge_maker(name)
+  #name.each do |i|
+     "Hello, my name is #{name}."
+  #end
+end
+
+def batch_badge_creator(attendees)
+   attendees.each do |i|
+      "Hello, my name is #{i}."
+   end
+end
+
+def assign_rooms(attendees)
+
+end
+
+def printer(attendees)
+
+end
