d0d06fc4c43aa1cbe074661ad9fba28e75f7a207
AHMED AFANDY <eng.computer1@yahoo.com>
2020-03-05 00:08:04 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,7 +7,7 @@ end
 
 def batch_badge_creator(attendees)
     attendees.each do |i|
-     attendees[attendees.index(i)] = "Hello, my name is #{i}."
+    attendees[attendees.index(i)] = "Hello, my name is #{i}."
    end
    return attendees
 end
@@ -20,5 +20,8 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-
+  attendees.each do |i|
+     puts "Hello, my name is #{i}."
+     puts "Hello, #{i}! You'll be assigned to room #{attendees.index(i)+1}!"
+  end
 end
